bbd90eae7b415b181845f8f7e074d21650904189
Arun2413 <gogulsakthivel@gmail.com>
2023-01-31 16:50:52 +0530

    categorysetup

--- Soolya_project/src/App.js
+++ Soolya_project/src/App.js
@@ -18,7 +18,7 @@ import ForgetPassword from './js_files/forgetPassword';
 import EmployeeProfile from './js_files/Employee-Profile';
 import Category from './js_files/category';
 import SubCategory from './js_files/subCategory';
-import Categotysetup from './Categotysetup';
+import Categorysetup from './Categorysetup';
 import Admin from './Admin';
 import Cart from './js_files/Cart';
 
@@ -83,7 +83,7 @@ function App() {
                 <Route path="/sign_in" element={<SignIn setTog={setTog}></SignIn>}></Route>
                 <Route path="service_man" element={<Serviceman></Serviceman>}></Route>
                 <Route path="/admin" element={<Admin></Admin>}></Route>
-                <Route path='/categorysetup' element={<Categotysetup></Categotysetup>}></Route>
+                <Route path="/categorysetup" element={<Categorysetup></Categorysetup>}></Route>
                 {/* {array.map((arr)=>(
                 <Route path={`/${arr.type || arr.district}`} element={<Plumbing array={array}></Plumbing>}></Route>
                  ))} */}
@@ -102,7 +102,7 @@ function App() {
                 <Route path="/sub_category" element={<SubCategory></SubCategory>}></Route>
             </Routes>
             <br></br>
-            <Footer></Footer>
+            {/* <Footer></Footer> */}
             
          </div>
          </AppContext.Provider>
