59b6ef3b0be3d0f7fa27b85fb22ef47880193429
Arun2413 <gogulsakthivel@gmail.com>
2023-02-01 10:26:31 +0530

    Categorysetup form

--- Soolya_project/src/Categorysetup.js
+++ Soolya_project/src/Categorysetup.js
@@ -1,38 +1,38 @@
-import React, { useEffect } from 'react'
+// import React, { useEffect } from 'react'
 import "./Admin.css"
 import Navbar_admin from './Navbar_admin';
-import { useState } from 'react';
-import axios from 'axios';
+// import { useState } from 'react';
+// import axios from 'axios';
 
 function Categorysetup() {
 
-  const [catagortSetup,setCatagortSetup]=useState("");
-  const [img,setImg]=useState("");
+//   const [catagortSetup,setCatagortSetup]=useState("");
+//   const [img,setImg]=useState("");
 
 
-  const [getData,setgetData]=useState([])
+//   const [getData,setgetData]=useState([])
 
-  useEffect(()=>{
-    axios.get("http://localhost:3001/api/fetch_items").then((res)=>{
-    setgetData(res.data);
-    })
+//   useEffect(()=>{
+//     axios.get("http://localhost:3001/api/fetch_items").then((res)=>{
+//     setgetData(res.data);
+//     })
 
-  },[getData])
+//   },[getData])
 
-const AddService = (e)=>{
-  e.preventDefault()
-  const formdata= new FormData()
-  formdata.append("catagortSetup",catagortSetup);
-  formdata.append("file",img)
-  axios.post("http://localhost:3001/api/new_catagory",formdata).then((res) => {
-    alert(res.data)
-   })
-}
-const localpath = "http://localhost:3001/"
+// const AddService = (e)=>{
+//   e.preventDefault()
+//   const formdata= new FormData()
+//   formdata.append("catagortSetup",catagortSetup);
+//   formdata.append("file",img)
+//   axios.post("http://localhost:3001/api/new_catagory",formdata).then((res) => {
+//     alert(res.data)
+//    })
+// }
+// const localpath = "http://localhost:3001/"
   return (
     <div className='ak'>
       <Navbar_admin></Navbar_admin>
-      <div className="container-fluid">
+      {/* <div className="container-fluid">
         <h1>Category Setup</h1>
         <form onSubmit={AddService}>
          <label>Category Name</label><br/>
@@ -49,7 +49,7 @@ const localpath = "http://localhost:3001/"
             <img src={localpath + data.filename} style={{width:"5em",height:"5em"}} alt=".........."></img>
           </div>
         ))
-      }
+      } */}
     </div>
   )
 }
