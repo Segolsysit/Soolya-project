359eae87dabd95335e48c40a17c43a60e95e1ba7
vignesh727 <vigneshvignesh4727@gmail.com>
2023-02-25 18:35:19 +0530

    media query in home page

--- Soolya_project/src/footer.js
+++ Soolya_project/src/footer.js
@@ -15,11 +15,14 @@ function Footer(){
 
         <div className="footer_content">
             <div className="left_div">
-                <h2>About us</h2>
-                <div className="div_content">
-                    <p>
-                        From Bikes and Autos to Prime Sedans and Prime SUVs, you will find a ride in your budget at your convenience any time.
-                    </p>
+                <h2 className="left_div_title">About us</h2>
+                <div className="div_content" id="div_content">
+                    <div>
+                        <h2 className="left_div_title_mobile">About us</h2>
+                        <p>
+                            From Bikes and Autos to Prime Sedans and Prime SUVs, you will find a ride in your budget at your convenience any time.
+                        </p>
+                    </div>
                     <div className="social_links">
                         <div>
                             <h4>Social links</h4>
@@ -39,41 +42,43 @@ function Footer(){
                     </div>
                 </div>
             </div>
-            <div className="center_div">
-                <h2>Address</h2>
-                <div className="div_content">
-                    <div className="place">
-                        <i className="fa-solid fa-location-dot"></i>
-                        <span>Rangampalayam,Erode</span>
-                    </div>
-                    <div className="phone">
-                        <i className="fa-solid fa-phone"></i>
-                        <span>+91-7674872834</span>
-                    </div>
-                    <div className="email">
-                        <i className="fa-solid fa-envelope"></i>
-                        <span>abc@gmail.com</span>
+            <div className="last_two_div">
+                <div className="center_div">
+                    <h2>Address</h2>
+                    <div className="div_content">
+                        <div className="place">
+                            <i className="fa-solid fa-location-dot"></i>
+                            <span id="div_content_span_width">Rangampalayam,Erode</span>
+                        </div>
+                        <div className="phone">
+                            <i className="fa-solid fa-phone"></i>
+                            <span>+91-7674872834</span>
+                        </div>
+                        <div className="email">
+                            <i className="fa-solid fa-envelope"></i>
+                            <span>abc@gmail.com</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="right_div">
-                <h2>Contact us</h2>
+                <div className="right_div">
+                    <h2>Contact us</h2>
 
-                <div>
-                    <div className="contact_us_head ">Email *</div>
-                    <input className="email_box form-control" type="email" required></input>
-                </div>
-                <div>
-                    <div className="contact_us_head">Message *</div>
-                    <textarea row="3" col="20" className='form-control'>
-                    </textarea>
-                </div>
-                <div>
-                    <button type="submit" className="footer_button btn btn-outline-primary">
-                        send
-                    </button>
-                </div>
+                    <div>
+                        <div className="contact_us_head ">Email *</div>
+                        <input className="email_box form-control" type="email" required></input>
+                    </div>
+                    <div>
+                        <div className="contact_us_head">Message *</div>
+                        <textarea row="3" col="20" className='form-control'>
+                        </textarea>
+                    </div>
+                    <div>
+                        <button type="submit" className="footer_button btn btn-outline-primary">
+                            send
+                        </button>
+                    </div>
 
+                </div>
             </div>
         </div>
      
--- Soolya_project/src/js_files/dashboard.js
+++ Soolya_project/src/js_files/dashboard.js
@@ -1,214 +1,75 @@
-// import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import "../css_files/dashboard.css";
-// import Header from "../header";
-// import { logOut } from "./auth";
-// import { removeUserData1 } from "./storage";
-import ReCAPTCHA from "react-google-recaptcha";
-import swal from 'sweetalert';
+
 
 
 
 
 function DashBoard(){
 
-    // const nav = useNavigate();
 
-    // const logOutUser = () => {
-    //     logOut();
-    //     nav("/sign_in");
+    const [amount, setAmount] = useState("");
+    const handlePayment = (e)=>{
+          e.preventDefault();
 
-    // }
+          if(amount === ""){
+              alert("Please enter amount");
+          }
+          else{
+            // alert("payment gateway open");
+
+            var options = {
+                key:"rzp_test_1SnQnLm783h5Op",
+                key_secret:"W3x1XiUXiyqIKQJrSBqaXGmE",
+                amount:amount *100,
+                currency:"INR",
+                name:"SOOLYA",
+                description:"Payment here",
+                handler:function(res){
+                    console.log(res);
+                    alert(res.razorpay_payment_id);
+                },
+                prefill:{
+                    name:'vignesh',
+                    email:"vigneshvignesh4727@gmail.com",
+                    contact:"9791823953"
+                },
+                notes:{
+                    address:"Segolsys software solutions"
+                },
+                theme:{
+                    color:"#3399cc"
+                }
+            };
+
+            var pay = new window.Razorpay(options);
+
+            pay.open();
+          }
+    }
+
 
     
-    
-    // const initialErrors_1 = {
-    //     email_input: null,
-    //     custom_error: null
-    // };
-  
-    // const [wrong, setWrong] = useState(initialErrors_1);
-  
-  
-    // const [dataLoading, settDataLoading] = useState(false);
-  
-    // const [email_input, setEmail_Input] = useState("");
-  
-  
-    // const handleForgetsubmit = (event) => {
-    //     event.preventDefault();
-  
-    //     let wrong = initialErrors_1;
-  
-  
-    //     if (email_input === "")
-    //         wrong.email_input = "Registered Email is required";
-  
-    //     if(email_input !== ""){
-    //             settDataLoading(true);
-    //             ResetApi(email_input).then((response)=>{
-    //                console.log(response);
-    //             }).catch((err)=>{
-  
-    //             if(err.code === "ERR_BAD_REQUEST"){
-    //                 setWrong({...wrong,custom_error:"Registered email is required"})   
-    //             }
-    //             })
-    //     }
-  
-    //     setWrong({ ...wrong })
-    // }
-
-
-//    const demo = (setBtn)=>{
-
-//     if (isAuthenticatedLogin()) {
-        
-//         setBtn(false);
-        
-//         return <Navigate to="/dashboard"></Navigate>
-//         return <Navigate to="/"></Navigate>
-//     }
-
-//    } 
-
-
-
-
-// jquery for step form
-
-
-
-
-   
-// $(document).ready(function(){
-    
-//     var current_fs, next_fs, previous_fs; //fieldsets
-//     var opacity;
-    
-//     $(".next").click(function(){
-        
-//         current_fs = $(this).parent();
-//         next_fs = $(this).parent().next();
-        
-//         //Add Class Active
-//         $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-        
-//         //show the next fieldset
-//         next_fs.show(); 
-//         //hide the current fieldset with style
-//         current_fs.animate({opacity: 0}, {
-//             step: function(now) {
-//                 // for making fielset appear animation
-//                 opacity = 1 - now;
-    
-//                 current_fs.css({
-//                     'display': 'none',
-//                     'position': 'relative'
-//                 });
-//                 next_fs.css({'opacity': opacity});
-//             }, 
-//             duration: 600
-//         });
-//     });
-    
-//     $(".previous").click(function(){
-        
-//         current_fs = $(this).parent();
-//         previous_fs = $(this).parent().prev();
-        
-//         //Remove class active
-//         $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-        
-//         //show the previous fieldset
-//         previous_fs.show();
-    
-//         //hide the current fieldset with style
-//         current_fs.animate({opacity: 0}, {
-//             step: function(now) {
-//                 // for making fielset appear animation
-//                 opacity = 1 - now;
-    
-//                 current_fs.css({
-//                     'display': 'none',
-//                     'position': 'relative'
-//                 });
-//                 previous_fs.css({'opacity': opacity});
-//             }, 
-//             duration: 600
-//         });
-//     });
-    
-//     $('.radio-group .radio').click(function(){
-//         $(this).parent().find('.radio').removeClass('selected');
-//         $(this).addClass('selected');
-//     });
-    
-//     $(".submit").click(function(){
-//         return false;
-//     })
-        
-//     });
-
-const onChange=(value)=>{
-    localStorage.setItem("captcha", value)
-  }
-
-
-  const captchaValue = ()=>{
-      return localStorage.getItem("captcha")
-  }
-
-  const authenticate = ()=>{
-      captchaValue() ===""?true:false;
-  }
-
-  const captchaSubmit = (e)=>{
-
-        e.preventDefault();
-
-
-      if (!authenticate()){
-        swal({
-            title: "Good job!",
-            text: "You clicked the button!",
-            icon: "success",
-            button: "Aww yiss!",
-          });
-      }
-      else{
-      alert("error");
-
-      }
-
-  }
-
-  
-
-   
 
     return(
         <div>
 
-            {/* <Header></Header> */}
-{/* 
-            <div className="demo_div">           
-                    <h1 className="demo_h1">Welcome to Dashboard</h1>
+
+        <div className="demo_h1">
+
+            <div>
+           
+
+                <input type="text" value={amount} onChange={(e)=>{setAmount(e.target.value)}}></input>
+                <br/>
+                <br/>
+                <div className="three">
+                    <button  type="button" onClick={handlePayment} >Submit</button>
+                </div>
+
             </div>
-            <div className="demo_h1">
-                <button type="button" >
-                    Logout
-                </button>
-            </div> */}
 
-
-                                <form onSubmit={captchaSubmit}>
-                                    <ReCAPTCHA
-                                            sitekey="6LdyhYIkAAAAAJj04Umnf4rQ427h49pItJtiBJ_l"
-                                            onChange={onChange}
-                                    />
-                                    <button type="submit">Confirm order</button>
-
-                                </form>
+        </div>
 
         </div>
     )
